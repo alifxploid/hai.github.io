cb7df27059f09952d1c95d05347b9cf553a5bfeb
alifxploid <69151185+alifxploid@users.noreply.github.com>
2022-12-13 17:43:25 +0700

    Update gai.js

--- gai.js
+++ gai.js
@@ -1,4 +1,3 @@
-<script type="text/javascript">
 	atOptions = {
 		'key' : '29eece38e5769b66a44412bdc5faa12e',
 		'format' : 'iframe',
@@ -7,4 +6,3 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://heartachegrabbedlaunching.com/29eece38e5769b66a44412bdc5faa12e/invoke.js"></scr' + 'ipt>');
-</script>
